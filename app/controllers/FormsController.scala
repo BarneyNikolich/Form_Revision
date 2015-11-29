4037f5a0aaf336c7e20ae0d7c3d938380ef625d1
Adam Trousdale <adam.trousdale@sky.com>
2015-11-29 11:07:05 +0000

    initial commit

---
+++ app/controllers/FormsController.scala
@@ -0,0 +1,43 @@
+package controllers
+
+import models.TestForm
+import play.api.mvc.{Action, Controller}
+
+/**
+ * Created by AdamT on 28/11/2015.
+ */
+trait FormsController extends Controller {
+
+  def view = Action {
+    Ok(views.html.forms.forms(TestForm.form))
+  }
+
+  def submit = Action { implicit request =>
+    TestForm.form.bindFromRequest.fold(
+      errorForm => {
+        BadRequest(views.html.forms.forms(errorForm))
+      },
+      formData => {
+        val results = List(
+          s"textInput: ${formData.textInput}",
+          s"optionalTextInput: ${formData.optionalTextInput}",
+          s"numberInput: ${formData.numberInput}",
+          s"selectInput: ${formData.selectInput}",
+          s"radioInput: ${formData.radioGroupInput}",
+          s"dateInput: ${formData.dateInput}",
+          s"passwordInput: ${formData.passwordInput}",
+          s"confirmPasswordInput: ${formData.confirmPasswordInput}",
+          s"checkboxInput: ${formData.checkboxInput}"
+        )
+        Redirect(routes.FormsController.formResults(results))
+      }
+    )
+  }
+
+  def formResults(filledForm: List[String]) = Action {
+    Ok(views.html.forms.formResults(filledForm))
+  }
+
+}
+
+object FormsController extends FormsController
---
+++ app/models/TestForm.scala
@@ -0,0 +1,42 @@
+package models
+
+import java.io.File
+
+import org.joda.time.LocalDate
+import play.api.data._
+import play.api.data.Forms._
+
+/**
+ * Created by AdamT on 28/11/2015.
+ */
+case class TestForm(textInput: String,
+                    optionalTextInput: Option[String],
+                    numberInput: Int,
+                    selectInput: String,
+                    radioGroupInput: String,
+                    dateInput: LocalDate,
+                    passwordInput: String,
+                    confirmPasswordInput: String,
+                    checkboxInput: Boolean)
+
+object TestForm {
+
+  val form = Form(
+    mapping(
+      "textInput" -> nonEmptyText,
+      "optionalTextInput" -> optional(text),
+      "numberInput" -> number,
+      "selectInput" -> text.verifying("Please select an option!", i => !i.equals("-- select --")),
+      "radioGroupInput" -> text,
+      "dateInput" -> jodaLocalDate.verifying("Date cant be in the future!", d => d.isBefore(LocalDate.now.plusDays(1))),
+      "passwordInput" -> text(8, 30),
+      "confirmPasswordInput" -> text(8, 30),
+      "checkboxInput" -> boolean.verifying("Must click this checkbox", c => c.equals(true))
+    )(TestForm.apply)(TestForm.unapply) verifying("Passwords must match!", fields => fields.passwordInput.equals(fields.confirmPasswordInput))
+    //match {
+     // case testForm => testForm.passwordInput.equals(testForm.confirmPasswordInput)
+    //})
+  )
+
+}
+
